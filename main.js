b74ee1f7d630db14fa784261e3fde36566dd1e4d
henrique <henrique@teste.com>
2024-08-13 21:11:56 -0300

    Criação de lista de tarefas

--- main.js
+++ main.js
@@ -1,34 +1,17 @@
-const formCount = document.getElementById("form-count");
-const btnAgain = document.getElementById("btn-again");
+$(document).ready(function(){
 
+    $('form').on('submit', function(e){
+        e.preventDefault();
 
-formCount.addEventListener("submit", function(e){
-    e.preventDefault();
+        const nomeTarefa = $('#input-tarefa').val();
+        const linhaTarefa = $(`<li><span>${nomeTarefa}<span></li>`);
 
-    const campoA = document.getElementById("campo-a");
-    const campoB = document.getElementById("campo-b"); 
-    const campoResp = document.getElementById("resp");
-    const btnCalc = document.getElementById("btn-calc");  
-    
-    let numCampoA = parseInt(campoA.value)
-    let numCampoB = parseInt(campoB.value)
+        linhaTarefa.prependTo('.lista-tarefas');
 
-    if(numCampoB > numCampoA){
-        campoResp.innerHTML = `O Campo B, com valor de ${numCampoB}, é maior que o campo A, de valor ${numCampoA}. Você passou no teste!`;
-        campoResp.classList.toggle("respSucesso");
-    }else if (parseInt(numCampoB) === numCampoA){
-        campoResp.innerHTML = `O Campo B, com valor de ${numCampoB}, é igual do campo A. Tente novamente`;
-        campoResp.classList.toggle("respErrado");
-    }else{
-        campoResp.innerHTML = `O Campo B, com valor de ${numCampoB}, é menor que o campo A, de valor ${numCampoA}. Tente novamente`;
-        campoResp.classList.toggle("respErrado");
-    }
-    campoA.value = "";
-    campoB.value = "";
-    btnCalc.classList.add("btn-disabled")
-    btnCalc.disabled = true;  
-    
-    btnAgain.addEventListener("click", function(){
-        location.reload()
+        $('#input-tarefa').val('');
     })
-})
+
+    $('.lista-tarefas').on('click', 'span', function(){
+        $(this).toggleClass('line')
+    });
+});
